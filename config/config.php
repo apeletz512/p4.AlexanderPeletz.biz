b114313cbcc2c01122bc0dec6ec8c44cc7bdc6cc
Alexander Peletz <APeletz512@gmail.com>
2013-12-14 17:48:07 -0500

    fixed htaccess issue and set up local DB

--- config/config.php
+++ config/config.php
@@ -14,7 +14,7 @@ If you want a different default time format for this app, set it below
 */
 
 # What is the name of this app?
-	define('APP_NAME', 'the Social System');  
+	define('APP_NAME', 'Trapper Keeper RX');  
 
 # When email is sent out from the server, where should it come from?
 # Ideally, this should match the domain name
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'apeletz512@gmail.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', 'alexan22_p2_AlexanderPeletz_biz'); 
+	define('DB_NAME', 'alexan22_p4_AlexanderPeletz_biz'); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
