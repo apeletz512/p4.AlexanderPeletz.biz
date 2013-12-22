5cb2e9f2af3fd9082c73b57129f8292c80882695
Alexander Peletz <APeletz512@gmail.com>
2013-12-22 15:16:09 -0500

    getting validation set up

--- config/config.php
+++ config/config.php
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'apeletz512@gmail.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', 'alexan22_p4_AlexanderPeletz_biz'); 
+	define('DB_NAME', 'alexan22_p4_alexanderpeletz_biz'); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
